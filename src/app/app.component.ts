6befdb61db81c9acba1ae058bfe08e857b6c9c43
Amatya <pratik.amatya@cotiviti.com>
2023-04-18 15:03:04 +0545

    horizontal menubar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
   title = 'pratik';
+  now = new Date().toLocaleTimeString()
+  timeUpdateInterval!: any
+
+  ngOnInit(): void {
+    this.timeUpdateInterval = setInterval(this.updateTime,1000)
+  }
+
+  updateTime(){
+    this.now = new Date().toLocaleTimeString()
+    // console.log('this.now: ',this.now); 
+    return this.now
+  }
+
+  ngOnDestroy(){
+    clearInterval(this.timeUpdateInterval)
+  }
 }
